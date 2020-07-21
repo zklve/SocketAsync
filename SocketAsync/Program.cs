0db82139109406ed01b40977942a728ef4972f6c
zkl <1090890128@qq.com>
2020-07-21 14:32:00 +0800

    添加项目文件。

---
+++ SocketAsync/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SocketAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Socket服务端启动，端口号:30011");
+            SocketServer server = new SocketServer();
+            server.StartListen();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketAsync/SocketClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketAsync
+{
+    public class SocketClient
+    {
+    }
+}
